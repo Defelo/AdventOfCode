3f42c261c9e85c1642e1592e05f60162dd4db578
Defelo <elodef42@gmail.com>
2021-12-20 14:10:17 +0100

    Added tests to rust solutions

--- 2021/2021.rs
+++ 2021/2021.rs
@@ -4,6 +4,7 @@
 
 #![feature(div_duration)]
 
+use std::ops::AddAssign;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
@@ -84,54 +85,88 @@ mod d24;
 mod d25;
 
 
+struct Day {
+    day: i32,
+    func: fn() -> (String, String),
+    part1: &'static str,
+    part2: &'static str,
+}
+
+struct DayResult<'a> {
+    day: &'a Day,
+    part1: String,
+    part2: String,
+    duration: Duration,
+}
+
+fn print_result(part: u8, actual: &String, expected: &str, total: &mut usize, failed: &mut usize) {
+    if expected.is_empty() {
+        println!("\x1b[33m skipped\x1b[0m | Part {}: \x1b[33m{}\x1b[0m", part, actual);
+    } else if actual == expected {
+        total.add_assign(1);
+        println!("\x1b[32m\x1b[1m✔ passed\x1b[0m | Part {}: \x1b[32m{}\x1b[0m", part, actual);
+    } else {
+        total.add_assign(1);
+        failed.add_assign(1);
+        println!("\x1b[31m\x1b[1m✘ failed\x1b[0m | Part {}: \x1b[31m{}\x1b[0m -> \x1b[32m{}\x1b[0m", part, actual, expected);
+    }
+}
+
 fn main() {
     println!("##### Advent of Code 2021 #####");
     let mut results = vec![];
-    for (day, func) in &[
-        (01, d01::run as fn() -> (String, String)),
-        (02, d02::run as fn() -> (String, String)),
-        (03, d03::run as fn() -> (String, String)),
-        (04, d04::run as fn() -> (String, String)),
-        (05, d05::run as fn() -> (String, String)),
-        (06, d06::run as fn() -> (String, String)),
-        (07, d07::run as fn() -> (String, String)),
-        (08, d08::run as fn() -> (String, String)),
-        (09, d09::run as fn() -> (String, String)),
-        (10, d10::run as fn() -> (String, String)),
-        (11, d11::run as fn() -> (String, String)),
-        (12, d12::run as fn() -> (String, String)),
-        (13, d13::run as fn() -> (String, String)),
-        (14, d14::run as fn() -> (String, String)),
-        (15, d15::run as fn() -> (String, String)),
-        (16, d16::run as fn() -> (String, String)),
-        (17, d17::run as fn() -> (String, String)),
-        (18, d18::run as fn() -> (String, String)),
-        (19, d19::run as fn() -> (String, String)),
-        (20, d20::run as fn() -> (String, String)),
-        (21, d21::run as fn() -> (String, String)),
-        (22, d22::run as fn() -> (String, String)),
-        (23, d23::run as fn() -> (String, String)),
-        (24, d24::run as fn() -> (String, String)),
-        (25, d25::run as fn() -> (String, String)),
+    let mut total = 0;
+    let mut failed = 0;
+    for day in &[
+        Day { day: 01, func: d01::run, part1: "1266", part2: "1217" },
+        Day { day: 02, func: d02::run, part1: "1648020", part2: "1759818555" },
+        Day { day: 03, func: d03::run, part1: "3895776", part2: "7928162" },
+        Day { day: 04, func: d04::run, part1: "49860", part2: "24628" },
+        Day { day: 05, func: d05::run, part1: "5698", part2: "15463" },
+        Day { day: 06, func: d06::run, part1: "345793", part2: "1572643095893" },
+        Day { day: 07, func: d07::run, part1: "340052", part2: "92948968" },
+        Day { day: 08, func: d08::run, part1: "392", part2: "1004688" },
+        Day { day: 09, func: d09::run, part1: "425", part2: "1135260" },
+        Day { day: 10, func: d10::run, part1: "278475", part2: "3015539998" },
+        Day { day: 11, func: d11::run, part1: "1659", part2: "227" },
+        Day { day: 12, func: d12::run, part1: "3679", part2: "107395" },
+        Day { day: 13, func: d13::run, part1: "747", part2: "ARHZPCUH" },
+        Day { day: 14, func: d14::run, part1: "3247", part2: "4110568157153" },
+        Day { day: 15, func: d15::run, part1: "366", part2: "2829" },
+        Day { day: 16, func: d16::run, part1: "906", part2: "819324480368" },
+        Day { day: 17, func: d17::run, part1: "3916", part2: "2986" },
+        Day { day: 18, func: d18::run, part1: "4469", part2: "4770" },
+        Day { day: 19, func: d19::run, part1: "403", part2: "10569" },
+        Day { day: 20, func: d20::run, part1: "5419", part2: "17325" },
+        Day { day: 21, func: d21::run, part1: "", part2: "" },
+        Day { day: 22, func: d22::run, part1: "", part2: "" },
+        Day { day: 23, func: d23::run, part1: "", part2: "" },
+        Day { day: 24, func: d24::run, part1: "", part2: "" },
+        Day { day: 25, func: d25::run, part1: "", part2: "" },
         
     ] {
-        if !Path::new(format!("2021/{:02}.txt", day).as_str()).exists() {continue;}
+        if !Path::new(format!("2021/{:02}.txt", day.day).as_str()).exists() {continue;}
         let s = Instant::now();
-        let (part1, part2) = func() as (String, String);
-        results.push((day, part1, part2, s.elapsed()));
+        let (part1, part2) = (day.func)() as (String, String);
+        results.push(DayResult {day, part1, part2, duration: s.elapsed()});
     }
 
-    let total_duration = results.iter().map(|(_, _, _, duration)| {
+    let total_duration = results.iter().map(|DayResult {duration, .. }| {
         duration
     }).sum::<Duration>();
 
-    for (day, part1, part2, duration) in results {
+    for DayResult {day, part1, part2, duration} in results {
         println!();
-        println!("=== Day {} ===", day);
-        println!("Part 1: {}", part1);
-        println!("Part 2: {}", part2);
+        println!("=== Day {} ===", day.day);
+        print_result(1, &part1, day.part1, &mut total, &mut failed);
+        print_result(2, &part2, day.part2, &mut total, &mut failed);
         println!("  => {:.6} ms ({:.2}%)", duration.as_secs_f64() * 1000f64, duration.div_duration_f64(total_duration) * 100f64);
     }
     println!();
+    if failed == 0 {
+        println!("\x1b[32m\x1b[1m✔ {}/{} passed\x1b[0m", total, total);
+    } else {
+        println!("\x1b[31m\x1b[1m✘ {}/{} failed\x1b[0m", failed, total);
+    }
     println!("=> {:.6} ms", total_duration.as_secs_f64() * 1000f64);
 }
--- templates/rust_year.rs
+++ templates/rust_year.rs
@@ -3,6 +3,7 @@
 {% endfor %}
 #![feature(div_duration)]
 
+use std::ops::AddAssign;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
@@ -11,31 +12,65 @@ use std::time::{Duration, Instant};
 mod d{{ '%02d' % n }};
 {% endfor %}
 
+struct Day {
+    day: i32,
+    func: fn() -> (String, String),
+    part1: &'static str,
+    part2: &'static str,
+}
+
+struct DayResult<'a> {
+    day: &'a Day,
+    part1: String,
+    part2: String,
+    duration: Duration,
+}
+
+fn print_result(part: u8, actual: &String, expected: &str, total: &mut usize, failed: &mut usize) {
+    if expected.is_empty() {
+        println!("\x1b[33m skipped\x1b[0m | Part {}: \x1b[33m{}\x1b[0m", part, actual);
+    } else if actual == expected {
+        total.add_assign(1);
+        println!("\x1b[32m\x1b[1m✔ passed\x1b[0m | Part {}: \x1b[32m{}\x1b[0m", part, actual);
+    } else {
+        total.add_assign(1);
+        failed.add_assign(1);
+        println!("\x1b[31m\x1b[1m✘ failed\x1b[0m | Part {}: \x1b[31m{}\x1b[0m -> \x1b[32m{}\x1b[0m", part, actual, expected);
+    }
+}
+
 fn main() {
     println!("##### Advent of Code {{ year }} #####");
     let mut results = vec![];
-    for (day, func) in &[
+    let mut total = 0;
+    let mut failed = 0;
+    for day in &[
         {% for n in range(1, 26) -%}
-            ({{ '%02d' % n }}, d{{ '%02d' % n }}::run as fn() -> (String, String)),
+            Day { day: {{ '%02d' % n }}, func: d{{ '%02d' % n }}::run, part1: "", part2: "" },
         {% endfor %}
     ] {
-        if !Path::new(format!("{{ year }}/{:02}.txt", day).as_str()).exists() {continue;}
+        if !Path::new(format!("{{ year }}/{:02}.txt", day.day).as_str()).exists() {continue;}
         let s = Instant::now();
-        let (part1, part2) = func() as (String, String);
-        results.push((day, part1, part2, s.elapsed()));
+        let (part1, part2) = (day.func)() as (String, String);
+        results.push(DayResult {day, part1, part2, duration: s.elapsed()});
     }
 
-    let total_duration = results.iter().map(|(_, _, _, duration)| {
+    let total_duration = results.iter().map(|DayResult {duration, .. }| {
         duration
     }).sum::<Duration>();
 
-    for (day, part1, part2, duration) in results {
+    for DayResult {day, part1, part2, duration} in results {
         println!();
-        println!("=== Day {} ===", day);
-        println!("Part 1: {}", part1);
-        println!("Part 2: {}", part2);
+        println!("=== Day {} ===", day.day);
+        print_result(1, &part1, day.part1, &mut total, &mut failed);
+        print_result(2, &part2, day.part2, &mut total, &mut failed);
         println!("  => {:.6} ms ({:.2}%)", duration.as_secs_f64() * 1000f64, duration.div_duration_f64(total_duration) * 100f64);
     }
     println!();
+    if failed == 0 {
+        println!("\x1b[32m\x1b[1m✔ {}/{} passed\x1b[0m", total, total);
+    } else {
+        println!("\x1b[31m\x1b[1m✘ {}/{} failed\x1b[0m", failed, total);
+    }
     println!("=> {:.6} ms", total_duration.as_secs_f64() * 1000f64);
 }
