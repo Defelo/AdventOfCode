758f66e31d4270976a5cc710c66d2594d200f330
Defelo <elodef42@gmail.com>
2022-12-02 07:00:03 +0100

    Add 2022/01.rs

---
+++ 2022/01.rs
@@ -0,0 +1,35 @@
+type Input = Vec<Vec<u32>>;
+
+fn get_input(input: &str) -> Input {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|elf| elf.split_whitespace().map(|x| x.parse().unwrap()).collect())
+        .collect()
+}
+
+fn part1(input: &Input) -> u32 {
+    input.iter().map(|elf| elf.iter().sum()).max().unwrap()
+}
+
+fn part2(input: &Input) -> u32 {
+    let (a, b, c) = input
+        .iter()
+        .map(|elf| elf.iter().sum())
+        .fold((0, 0, 0), |(a, b, c), x| {
+            if x > b {
+                if x > a {
+                    (x, a, b)
+                } else {
+                    (a, x, b)
+                }
+            } else if x > c {
+                (a, b, x)
+            } else {
+                (a, b, c)
+            }
+        });
+    a + b + c
+}
+
+aoc::main!(2022, 1);
