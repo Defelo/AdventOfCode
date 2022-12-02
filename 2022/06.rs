aaf9f2c0e1d2bdbddfa76fe6ca3ff181c9b70025
Defelo <elodef42@gmail.com>
2022-12-02 07:56:59 +0100

    Add rust templates

---
+++ 2022/06.rs
@@ -0,0 +1,15 @@
+type Input = ();
+
+fn get_input(input: &str) -> Input {
+    todo!()
+}
+
+fn part1(input: &Input) -> String {
+    todo!()
+}
+
+fn part2(input: &Input) -> String {
+    todo!()
+}
+
+aoc::main!(2022, 2);
