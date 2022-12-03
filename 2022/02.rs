5cffcdcd155654a737b81ff13b6e79a873cca696
Defelo <elodef42@gmail.com>
2022-12-03 06:45:47 +0100

    Improve 2022/02.rs

--- 2022/02.rs
+++ 2022/02.rs
@@ -1,119 +1,24 @@
-#[derive(Clone, Copy)]
-enum Shape {
-    Rock = 0,
-    Paper = 1,
-    Scissors = 2,
-}
-
-impl Shape {
-    fn outcome(&self, opponent: &Self) -> Outcome {
-        match (3 + *opponent as u8 - *self as u8) % 3 {
-            0 => Outcome::Draw,
-            1 => Outcome::Win,
-            2 => Outcome::Lose,
-            _ => unreachable!(),
-        }
-    }
-
-    fn choose_by_outcome(&self, outcome: &Outcome) -> Self {
-        match (*self as u8 + *outcome as u8) % 3 {
-            0 => Self::Rock,
-            1 => Self::Paper,
-            2 => Self::Scissors,
-            _ => unreachable!(),
-        }
-    }
-
-    fn score(&self) -> u32 {
-        *self as u32 + 1
-    }
-}
-
-#[derive(Clone, Copy)]
-enum Outcome {
-    Draw = 0,
-    Win = 1,
-    Lose = 2,
-}
-
-impl Outcome {
-    fn score(&self) -> u32 {
-        match self {
-            Self::Lose => 0,
-            Self::Draw => 3,
-            Self::Win => 6,
-        }
-    }
-}
-
-enum ShapeOrOutcome {
-    X,
-    Y,
-    Z,
-}
-
-impl From<&ShapeOrOutcome> for Shape {
-    fn from(soo: &ShapeOrOutcome) -> Self {
-        match soo {
-            ShapeOrOutcome::X => Self::Rock,
-            ShapeOrOutcome::Y => Self::Paper,
-            ShapeOrOutcome::Z => Self::Scissors,
-        }
-    }
-}
-
-impl From<&ShapeOrOutcome> for Outcome {
-    fn from(soo: &ShapeOrOutcome) -> Self {
-        match soo {
-            ShapeOrOutcome::X => Self::Lose,
-            ShapeOrOutcome::Y => Self::Draw,
-            ShapeOrOutcome::Z => Self::Win,
-        }
-    }
-}
-
-type Input = Vec<(Shape, ShapeOrOutcome)>;
+type Input = Vec<(u32, u32)>;
 
 fn get_input(input: &str) -> Input {
     input
         .trim()
         .split('\n')
         .map(|line| {
-            let a = match line.chars().next().unwrap() {
-                'A' => Shape::Rock,
-                'B' => Shape::Paper,
-                'C' => Shape::Scissors,
-                _ => panic!(),
-            };
-            let b = match line.chars().nth(2).unwrap() {
-                'X' => ShapeOrOutcome::X,
-                'Y' => ShapeOrOutcome::Y,
-                'Z' => ShapeOrOutcome::Z,
-                _ => panic!(),
-            };
-            (a, b)
+            (
+                (line.as_bytes()[0] - b'A') as u32,
+                (line.as_bytes()[2] - b'X') as u32,
+            )
         })
         .collect()
 }
 
 fn part1(input: &Input) -> u32 {
-    input
-        .iter()
-        .map(|(a, b)| {
-            let b = b.into();
-            a.outcome(&b).score() + b.score()
-        })
-        .sum()
+    input.iter().map(|(a, b)| (4 + b - a) % 3 * 3 + b + 1).sum()
 }
 
 fn part2(input: &Input) -> u32 {
-    input
-        .iter()
-        .map(|(a, b)| {
-            let b = b.into();
-            a.choose_by_outcome(&b).score() + b.score()
-        })
-        .sum()
+    input.iter().map(|(a, b)| (2 + a + b) % 3 + 1 + b * 3).sum()
 }
 
 aoc::main!(2022, 2);
