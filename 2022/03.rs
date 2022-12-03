87b3613ca231b1df7937ee6d17e7dc7d25e506c6
Defelo <elodef42@gmail.com>
2022-12-03 12:31:48 +0100

    Add 2022/03.rs

--- 2022/03.rs
+++ 2022/03.rs
@@ -1,15 +1,39 @@
-type Input = ();
+type Input = Vec<Vec<u8>>;
 
 fn get_input(input: &str) -> Input {
-    todo!()
+    input
+        .trim()
+        .split('\n')
+        .map(|line| line.as_bytes().into())
+        .collect()
 }
 
-fn part1(input: &Input) -> String {
-    todo!()
+fn to_num(x: u8) -> u8 {
+    x - 38 - (x >= 97) as u8 * 58
 }
 
-fn part2(input: &Input) -> String {
-    todo!()
+fn bytes_to_set(bytes: &[u8]) -> u64 {
+    bytes.iter().fold(0, |acc, x| acc | 1 << to_num(*x))
 }
 
-aoc::main!(2022, 2);
+fn part1(input: &Input) -> u32 {
+    input
+        .iter()
+        .map(|line| {
+            (bytes_to_set(&line[..line.len() / 2]) & bytes_to_set(&line[line.len() / 2..]))
+                .trailing_zeros()
+        })
+        .sum()
+}
+
+fn part2(input: &Input) -> u32 {
+    (0..input.len())
+        .step_by(3)
+        .map(|i| {
+            (bytes_to_set(&input[i]) & bytes_to_set(&input[i + 1]) & bytes_to_set(&input[i + 2]))
+                .trailing_zeros()
+        })
+        .sum()
+}
+
+aoc::main!(2022, 3);
