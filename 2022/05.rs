e52068efec12f12f17d687caffaa57cc08000510
Defelo <elodef42@gmail.com>
2022-12-05 09:55:48 +0100

    Improve 2022/05.rs

--- 2022/05.rs
+++ 2022/05.rs
@@ -1,5 +1,49 @@
 type Input = (Vec<String>, Vec<(usize, usize, usize)>);
 
+struct Solver<'input> {
+    stacks: &'input Vec<String>,
+    instructions: &'input Vec<(usize, usize, usize)>,
+    get_next: fn(cnt: usize, h: usize) -> usize,
+}
+
+impl<'input> Solver<'input> {
+    pub fn new(
+        (stacks, instructions): &'input Input,
+        get_next: fn(cnt: usize, h: usize) -> usize,
+    ) -> Self {
+        Self {
+            stacks,
+            instructions,
+            get_next,
+        }
+    }
+
+    fn get_crate(&self, ins: usize, st: usize, h: usize) -> char {
+        if ins == 0 {
+            return self.stacks[st].chars().nth_back(h).unwrap();
+        }
+        let (cnt, i, j) = self.instructions[ins - 1];
+        if st == j {
+            if h < cnt {
+                self.get_crate(ins - 1, i, (self.get_next)(cnt, h))
+            } else {
+                self.get_crate(ins - 1, st, h - cnt)
+            }
+        } else if st == i {
+            self.get_crate(ins - 1, st, h + cnt)
+        } else {
+            self.get_crate(ins - 1, st, h)
+        }
+    }
+
+    pub fn solve(self) -> String {
+        let n = self.instructions.len();
+        (0..self.stacks.len())
+            .map(|i| self.get_crate(n, i, 0))
+            .collect()
+    }
+}
+
 fn get_input(input: &str) -> Input {
     let (initial, instructions) = input.split_once("\n\n").unwrap();
     let mut it = initial.lines().rev();
@@ -34,26 +78,12 @@ fn get_input(input: &str) -> Input {
     (stacks, instructions)
 }
 
-fn part1((stacks, instructions): &Input) -> String {
-    let mut stacks = stacks.clone();
-    for (cnt, i, j) in instructions {
-        for _ in 0..*cnt {
-            let c = stacks[*i].pop().unwrap();
-            stacks[*j].push(c);
-        }
-    }
-    stacks.iter().map(|s| s.chars().last().unwrap()).collect()
+fn part1(input: &Input) -> String {
+    Solver::new(input, |cnt, h| cnt - h - 1).solve()
 }
 
-fn part2((stacks, instructions): &Input) -> String {
-    let mut stacks = stacks.clone();
-    for (cnt, i, j) in instructions {
-        let n = stacks[*i].len() - cnt;
-        let s = &stacks[*i][n..].to_owned();
-        stacks[*j].push_str(s);
-        stacks[*i].truncate(n);
-    }
-    stacks.iter().map(|s| s.chars().last().unwrap()).collect()
+fn part2(input: &Input) -> String {
+    Solver::new(input, |_, h| h).solve()
 }
 
 aoc::main!(2022, 5);
