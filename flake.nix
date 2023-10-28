eebb719bb16c9e34f47d6e8a791a877a36fa2395
Defelo <elodef42@gmail.com>
2023-10-28 17:42:15 +0200

    Convert shell.nix into flake

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+
+    downloadInput = pkgs.stdenvNoCC.mkDerivation {
+      name = "aoc-download-input";
+      dontUnpack = true;
+      nativeBuildInputs = with pkgs; [makeWrapper];
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ${./scripts/download_input.sh} $out/bin/aoc-download-input
+        chmod +x $out/bin/*
+        wrapProgram $out/bin/* --set PATH ${with pkgs;
+          lib.makeBinPath [
+            bash
+            coreutils
+            curl
+            gnugrep
+          ]}
+      '';
+    };
+    getSession = pkgs.python3.pkgs.buildPythonApplication {
+      name = "aoc-get-session";
+      pyproject = false;
+      dontUnpack = true;
+      installPhase = "mkdir -p $out/bin; cp ${./scripts/get_session.py} $out/bin/aoc-get-session; chmod +x $out/bin/*";
+      propagatedBuildInputs = with pkgs.python3.pkgs; [
+        requests
+        pycrypto
+      ];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        downloadInput
+        getSession
+      ];
+      packages = with pkgs; [
+        just
+        jq
+        hyperfine
+
+        # Python
+        (python311.withPackages (p:
+          with p; [
+            numpy
+            pyperclip
+          ]))
+
+        # Haskell
+        (haskellPackages.ghcWithPackages (p: with p; [regex-tdfa]))
+        haskell-language-server
+        ormolu # haskell code formatter
+      ];
+      PYTHONPATH = ".";
+    };
+  };
+}
