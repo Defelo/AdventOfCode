3dea7feefb7b5d9088fba23eb7caa2b093a2b041
Defelo <mail@defelo.de>
2024-11-30 12:51:08 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/3c5393024b6bb25ffe610901d892d2e189f24eb7?narHash=sha256-sE8KdzYOnG6NLukpdSOHhCXugr7R%2BLIzbvJ1jK6Yb/E%3D' (2024-11-06)
      → 'github:NixOS/nixpkgs/fa42b5a5f401aab8a32bd33c9a4de0738180dc59?narHash=sha256-RzaI1RO0UXqLjydtz3GAXSTzHkpb/lLD1JD8a0W4Wpo%3D' (2024-11-28)
    • Updated input 'uiua':
        'github:uiua-lang/uiua/92c14da2d4f691e9886852a0b6a6a9491d1aeedb?narHash=sha256-WCpnZBg/LELmoXtqSp4FxaFu58QLoM4sFuDaJmUh5Ec%3D' (2024-11-25)
      → 'github:uiua-lang/uiua/ebe12e66a8a63119fa0f4f64c2100476037d81cb?narHash=sha256-WMrcwMgc/sDWr1/7TD1WhNMrrmEB8uuB%2Bwe1TiZAelI%3D' (2024-11-30)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     uiua.url = "github:uiua-lang/uiua";
   };
 
@@ -28,7 +28,7 @@
                 ];
             };
 
-            python = pkgs.python311.withPackages (p:
+            python = pkgs.python313.withPackages (p:
               with p; [
                 z3
                 numpy
