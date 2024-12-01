037820b3fdcb20f5d3da2a605fdf0a214eef368e
Defelo <mail@defelo.de>
2024-12-01 20:07:53 +0100

    Rust/2019/02: add solution

---
+++ Rust/2019/02.rs
@@ -0,0 +1,40 @@
+#![feature(test)]
+
+use intcode::{Int, Vm};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+mod intcode;
+
+type Input = Vec<Int>;
+
+fn setup(input: &str) -> Input {
+    input
+        .split(',')
+        .map(|x| x.trim().parse().unwrap())
+        .collect()
+}
+
+fn run(input: &Input, noun: Int, verb: Int) -> Int {
+    let mut vm = Vm::new(input.iter().copied());
+    vm.write(1, noun);
+    vm.write(2, verb);
+    vm.run().unwrap();
+    vm.read(0)
+}
+
+fn part1(input: &Input) -> Int {
+    run(input, 12, 2)
+}
+
+fn part2(input: &Input) -> Int {
+    (0..100)
+        .into_par_iter()
+        .find_map_any(|noun| {
+            (0..100)
+                .find(|&verb| run(input, noun, verb) == 19690720)
+                .map(|verb| 100 * noun + verb)
+        })
+        .unwrap()
+}
+
+aoc::main!(2019, 2);
