2b69d66e7f4da88e45d2a8ed8b65b390db5b8496
Defelo <mail@defelo.de>
2024-12-03 06:18:21 +0100

    Rust/2024/03: add solution

---
+++ Rust/2024/03.rs
@@ -0,0 +1,34 @@
+#![feature(test)]
+
+use regex::Regex;
+
+type Input = String;
+
+fn setup(input: &str) -> Input {
+    input.trim().into()
+}
+
+fn part1(input: &Input) -> i64 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    re.captures_iter(input)
+        .map(|c| c[1].parse::<i64>().unwrap() * c[2].parse::<i64>().unwrap())
+        .sum()
+}
+
+fn part2(input: &Input) -> i64 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let mut enabled = true;
+    let mut out = 0;
+    for c in re.captures_iter(input) {
+        if &c[0] == "do()" {
+            enabled = true;
+        } else if &c[0] == "don't()" {
+            enabled = false;
+        } else if enabled {
+            out += c[1].parse::<i64>().unwrap() * c[2].parse::<i64>().unwrap();
+        }
+    }
+    out
+}
+
+aoc::main!(2024, 3, ex: 1[a], 2[b]);
